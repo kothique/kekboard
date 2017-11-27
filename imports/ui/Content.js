b66e21c2ef4c317906503be246aac1558f5e064e
Kothique Félix <kothique@protonmail.ch>
2017-11-27 03:50:00 +0300

    Add Main component

--- imports/ui/Content.js
+++ imports/ui/Content.js
@@ -1,15 +1,36 @@
 import React from 'react'
 
 import Header from './Header'
+import Main from './Main'
 import './Content.css'
 
+const defaultPage = 'main'
+
 class Content extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentPage: defaultPage
+    }
+  }
   render() {
     const { ...rest } = this.props;
+
+    switch (this.state.currentPage) {
+      case 'main':
+        currentPageHtml = <Main />
+        break;
+      default:
+        currentPageHtml = <h4 style={{ textAlign: 'center' }}>404 - Page Not Found</h4>
+        break;
+    }
 
     return (
       <div {...rest}>
         <Header id="header" />
+
+        {currentPageHtml}
       </div>
     )
   }
---
+++ imports/ui/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Main.css'
+
+class Main extends React.Component {
+  render() {
+    return (
+      <h4>Welcome to Kekboard!</h4>
+    )
+  }
+}
+
+export default Main
